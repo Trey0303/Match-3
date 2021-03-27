c64ea245ea47e80829efc210b1171e24dbe909cd
Trey Gleason <treygleason8@gmail.com>
2021-03-26 17:02:03 -0700

    working on player controller script
    adjust shape sizes, added box 2d colliders to shapes, and working on player controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector2 startMouseHoldPos;
+    private Vector2 endMouseHoldPos;
+
+    public float dragAngle = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        startMouseHoldPos = Input.mousePosition;
+        Debug.Log(startMouseHoldPos);
+    }
+}
