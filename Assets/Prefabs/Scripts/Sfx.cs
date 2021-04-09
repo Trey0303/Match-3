21ebe886863c5a4f396c7fa8b5d8647088f5cf5a
Trey Gleason <treygleason8@gmail.com>
2021-04-09 11:45:44 -0700

    planing to add sound effects
    moved script folder into prefab folder
    
    Also removed BackBoard script because wasn't used for anything and had to remove it from cube prefab to prevent warning errors from popping up.

--- Assets/Prefabs/Scripts/Sfx.cs
+++ Assets/Prefabs/Scripts/Sfx.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackBoard : MonoBehaviour
+public class Sfx : MonoBehaviour
 {
-    
-
     // Start is called before the first frame update
     void Start()
     {
