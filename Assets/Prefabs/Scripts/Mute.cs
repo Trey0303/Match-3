78edf9883c01100d2cd575c9086dbb8a8bc772c5
Trey Gleason <treygleason8@gmail.com>
2021-04-16 12:09:02 -0700

    added a mute button for background music
    will work on adding a mute button for sound when match is made. Also will work on getting settings icons to change to let player know if its muted or not as a visual reference

---
+++ Assets/Prefabs/Scripts/Mute.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mute : MonoBehaviour
+{
+    public bool isMuted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMuted = PlayerPrefs.GetInt("MUTED") == 1;
+        AudioListener.pause = isMuted;
+    }
+
+    public void MuteMusicPressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+        PlayerPrefs.GetInt("MUTED", isMuted ? 1 : 0);
+
+
+        //if (isMuted)
+        //{
+        //    background.Pause();
+        //}
+        //else
+        //{
+        //    background.Play();
+        //}
+    }
+}
--- Assets/Prefabs/Scripts/Sfx.cs
+++ Assets/Prefabs/Scripts/Sfx.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Sfx : MonoBehaviour
 {
+    public InputField muteButton;
     public AudioSource match;
     public AudioSource background;
+
+    public int isMuted;
+
+    void Start()
+    {
+        isMuted = PlayerPrefs.GetInt("MUTE");//grabs saved settings
+        if (isMuted == 0)
+        {
+            background.Pause();
+            PlayerPrefs.SetInt("MUTE", isMuted);
+            PlayerPrefs.Save();
+        }
+        else if (isMuted == 1)
+        {
+            background.Play();
+            PlayerPrefs.SetInt("MUTE", isMuted);
+            PlayerPrefs.Save();
+        }
+    }
 
     public void PlaySfx()
     {
@@ -20,5 +44,25 @@ public class Sfx : MonoBehaviour
     public void StopBackgroundMusic()
     {
         background.Stop();
+    }
+
+    public void MuteMusicPressed()
+    {
+        Debug.Log(isMuted);
+
+        if (isMuted == 1)//if unmuted then mute
+        {
+            background.Pause();//tells to mute/pause
+            isMuted = 0;//sets to muted state
+            PlayerPrefs.SetInt("MUTE", isMuted);//saves change to settings
+            
+        }
+        else if(isMuted == 0)//if muted then unmute
+        {
+            background.Play();//tells to play/unmute
+            isMuted = 1;//sets to unmuted state
+            PlayerPrefs.SetInt("MUTE", isMuted);//saves change to settings
+
+        }
     }
 }
